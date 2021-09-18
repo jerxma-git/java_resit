87adc6c850e51759b065f1275b0250bce18ed0b5
jerxxxma <mazheromskii@gmail.com>
2021-09-18 15:02:02 +0300

    remove Comparable requirement

--- parsort/ParallelSort.java
+++ parsort/ParallelSort.java
@@ -4,13 +4,13 @@ import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class ParallelSort<T extends Comparable<T>> {
+public class ParallelSort<T> {
     static boolean log = true;
 
     public ParallelSort(List<T> unsorted) {
         vals = new ArrayList<>(unsorted);
         temp = new ArrayList<>(unsorted);
-        comparator = Comparator.naturalOrder();
+        comparator = null;
     }
 
     public ParallelSort(List<T> unsorted, Comparator<T> comparator) {
@@ -25,11 +25,36 @@ public class ParallelSort<T extends Comparable<T>> {
     List<T> sort() {
         for (int step = 1; step < vals.size(); step *= 2) {
             for (int i = 0; (i + 1) * step < vals.size(); i += 2) {
+                // TODO: parallelize
                 mergeRange(i * step, (i + 1) * step, (i + 2) * step);
             }
         }
         return vals;
     }
+
+    // List<T> sort() {
+    //     for (int step = 1; step < vals.size(); step *= 2) {
+    //             List<Thread> threads = new ArrayList<>();
+    //         for (int i = 0; (i + 1) * step < vals.size(); i += 2) {
+    //             final int start = i * step;
+    //             final int mid = (i+1) * step;
+    //             final int end = (i+2) * step;
+    //             Thread th = new Thread( () -> mergeRange(start, mid, end) );
+    //             threads.add(th);
+    //             th.run();
+    //             // mergeRange(i * step, (i + 1) * step, (i + 2) * step);
+    //         }
+    //         for (Thread th : threads) {
+    //             try {
+    //                 th.join();
+    //             } catch (InterruptedException e) {
+    //                 //TODO: handle exception
+    //             }
+    //         }
+
+    //     }
+    //     return vals;
+    // }
 
     void mergeRange(int start, int mid, int end) {
         if (end > vals.size()) {
@@ -42,10 +67,10 @@ public class ParallelSort<T extends Comparable<T>> {
         int pos2 = mid;
         int pos = start;
         while (pos < end) {
-            while (pos1 < mid && (pos2 == end || comparator.compare(temp.get(pos1), temp.get(pos2)) <= 0)) {
+            while (pos1 < mid && (pos2 == end || compare(temp.get(pos1), temp.get(pos2)) <= 0)) {
                 vals.set(pos++, temp.get(pos1++));
             }
-            while (pos2 < end && (pos1 == mid || comparator.compare(temp.get(pos2), temp.get(pos1)) < 0)) {
+            while (pos2 < end && (pos1 == mid || compare(temp.get(pos2), temp.get(pos1)) < 0)) {
                 vals.set(pos++, temp.get(pos2++));
             }
         }
@@ -59,9 +84,9 @@ public class ParallelSort<T extends Comparable<T>> {
         Random rd = new Random();
         List<Integer> vals = rd.ints().limit(arraySize).boxed().collect(Collectors.toList());
         log(vals);
-        ParallelSort<Integer> ps = new ParallelSort<>(vals);
+        ParallelSort<Integer> ps = new ParallelSort<>(vals, Comparator.reverseOrder());
         List<Integer> psSorted = ps.sort();
-        vals.sort(Integer::compareTo);
+        vals.sort(Comparator.reverseOrder());
         log(psSorted);
         log(vals);
         return vals.equals(psSorted);
@@ -83,6 +108,11 @@ public class ParallelSort<T extends Comparable<T>> {
         if (log) {
             System.out.println(msg);
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    private int compare(T o1, T o2) {
+        return comparator == null ? ((Comparable<T>) o1).compareTo(o2) : comparator.compare(o1, o2);
     }
 
     public static void main(String[] args) {
